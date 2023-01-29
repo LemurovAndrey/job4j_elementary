9fb71b0fa639f6ad8c4b5cc886d803765d757738
LemurovAndrey <lemurovit@gmail.com>
2023-01-29 10:05:25 +0600

    4.1. Аргументы [#295521]

---
+++ src/main/java/ru/job4j/calculator/ArgMethod.java
@@ -0,0 +1,16 @@
+package ru.job4j.calculator;
+
+public class ArgMethod {
+
+    public static void hello(String name, int age) {
+        System.out.println("Hello " + name + " Age = " + age);
+    }
+
+    public static void main(String[] args) {
+        String name = "Job4j";
+        ArgMethod.hello(name, 6);
+        ArgMethod.hello(name, 6);
+        ArgMethod.hello(name, 6);
+        ArgMethod.hello(name, 6);
+    }
+}
